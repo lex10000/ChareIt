15b8f9767f69441d93d8b3811ffb1414efd82ec0
alexey <lex10000@yandex.ru>
2020-10-10 21:21:46 +0700

    big repairing

--- common/mail/emailVerify-html.php
+++ common/mail/emailVerify-html.php
@@ -7,9 +7,15 @@ use yii\helpers\Html;
 $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['user/default/verify-email', 'token' => $user->verification_token]);
 ?>
 <div class="verify-email">
-    <p>Hello <?= Html::encode($user->username) ?>,</p>
+    <p>Здравствуйте, <?= Html::encode($user->username) ?>,</p>
 
     <p>Для подтверждения регистрации пожалуйста перейдите по этой ссылке:</p>
 
     <p><?= Html::a(Html::encode($verifyLink), $verifyLink) ?></p>
+
+    <br>
+    <br>
+    <p>После подтверждения регистрации, вы можете изменить настройки профиля (аватарку, информацио о себе, сменить пароль)
+    в разделе "настройки"</p>
+    
 </div>
